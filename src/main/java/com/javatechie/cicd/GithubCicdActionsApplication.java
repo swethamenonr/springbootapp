b7b748d56774f4dfe3eabda4c0cd199ac7c513f6
Swetha Menon <100666112+swethamenonr@users.noreply.github.com>
2022-03-20 15:44:31 +0300

    app update

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,16 +11,8 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to javatechie !";
+		return "It works !";
 	}
-
-//	echo "# github-actions-example" >> README.md
-//	git init
-//	git add README.md
-//	git commit -m "first commit"
-//	git branch -M main
-//	git remote add origin https://github.com/Java-Techie-jt/github-actions-example.git
-//	git push -u origin main
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
 	}
